8927a5b6e8df43d07fc1406e01b45b6690a956cb
Austin Johnson <technicallyaustin@gmail.com>
2021-03-03 19:55:03 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -12,8 +12,8 @@ class Customer
     @@all
   end
   
-  def new_meal(waiter, self, total, tip)
-   Meal.new(waiter, self, total, tip=0)
+  def new_meal(waiter, total, tip=0)
+   Meal.new(waiter, self, total, tip)
   end
   
   def meals
